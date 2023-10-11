2d570d3f63f9629b78a665b4ec32a9f10edfc254
hasuhwan <hapung215@gmail.com>
2023-10-11 14:27:51 +0900

    add weahter ing

---
+++ components/Weather.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default function Weather() {
+  return (
+    <View>
+      <Text>hi</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
